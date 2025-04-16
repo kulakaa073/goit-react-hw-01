cef3d888001db21ff4532bb78aaf06922b7bf0c7
Shade <kulakaa073@gmail.com>
2025-04-16 19:21:22 +0300

    Adding base components

---
+++ src/componnents/FriendList/FriendList.jsx
@@ -0,0 +1,13 @@
+import FriendListItem from '/src/componnents/FriendListItem/FriendListItem';
+
+export default function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
+    </ul>
+  );
+}
